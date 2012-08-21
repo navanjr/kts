3ed5dd047f27fe411efe2e1c31a1bdfbd556f3d2
Nate Anderson <nate.anderson@kellpro.com>
2012-08-21 10:14:24 -0500

    key_commit: better core setup

--- setupKTSCore.bat
+++ setupKTSCore.bat
@@ -7,6 +7,10 @@ sqlcmd -H. -d master -Usa -P America#1 -Q"create database %dbname%"
 
 sqlcmd -H. -d %dbname% -Usa -P America#1 -i C:\client\key\kts\SqlObjects\enableAdvancedSQLOptions~Procedure~1.txt
 sqlcmd -H. -d %dbname% -Usa -P America#1 -Q"exec dbo.enableAdvancedSQLOptions"
+
 sqlcmd -H. -d %dbname% -Usa -P America#1 -i C:\client\key\kts\SqlObjects\keyCore~Script~2.txt
 
+sqlcmd -H. -d %dbname% -Usa -P America#1 -i C:\client\key\kts\SqlObjects\keySQLObjectDispatcher~Procedure~2.txt
+
 bcp %dbname%.dbo.template in "Needed\bcpTemplate.txt" -Usa -PAmerica#1 -c
+bcp %dbname%.dbo.Users in "Needed\bcpUsers.txt" -Usa -PAmerica#1 -c
