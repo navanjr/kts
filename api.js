c9630bac88d01f13d27b6e5678839f63a31df9e3
Nate Anderson <nate.anderson@kellpro.com>
2012-11-08 20:09:23 -0600

    key_commit: monster API commit, invoices and taxroll work, receipts dont

--- api.js
+++ api.js
@@ -10,13 +10,20 @@ var method = process.argv[3].toLowerCase();
 var host = process.argv[4].toLowerCase();
 var pathSeed = process.argv[5] + '.json';
 var data = process.argv[6];
-if (method == 'get'){
- var path = (process.argv[6])? pathSeed + '?' + data:pathSeed;
+if (data){
+    var n = data.split(':');
+    if (n[0] == 'file'){
+        var postFileName = n[1] + ':' + n[2];
+    }
 }
+
+if (method == 'get'){
+ var path = (data)? pathSeed + '?' + data:pathSeed;
+}
+
 if (method == 'post'){
  var path = pathSeed;
 }
-
 var options = {
   host: host,
   port: 80,
@@ -27,6 +34,21 @@ var options = {
 
 //console.log(options);
 
+function getDataForPost(filename){
+    var fs = require('fs');
+    var dataY = fs.readFile(filename, 'utf8', function (err,dataZ) {
+        if (err) {
+            return console.log(err);
+        }
+        if (method == 'get'){
+            getData();
+        }else{
+            postData(dataZ);
+        }
+    });
+}
+
+
 function getData(){
     var req = http.request(options, function(res) {
     //  console.log('STATUS: ' + JSON.stringify(res.statusCode));
@@ -34,6 +56,7 @@ function getData(){
       res.setEncoding('utf8');
       res.on('data', function (chunk) {
         console.log(chunk);
+        console.log('data I used: ',options.path);
       });
     });
 
@@ -41,7 +64,7 @@ function getData(){
       console.log('problem with request: ' + e.message);
     });
 
-    req.write(data + '\n');
+//    req.write(data + '\n');
     req.end();
 }
 
@@ -63,16 +86,23 @@ function postData(data){
           console.log(chunk);
         });
     });
-
     req.write(data);
     req.end();
 }
 
 // write data to request body
 if (method == 'get'){
-    getData();
+    if (postFileName){
+        getDataForPost(postFileName);
+    }else{
+        getData();
+    }
 }
 
 if (method == 'post'){
-    postData(data);
+    if (n[0] == 'file'){
+        getDataForPost(postFileName);
+    }else{
+        postData(data);
+    }
 }
