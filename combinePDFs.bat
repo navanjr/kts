609c6255dc34fd86c3d9b0005e0514fa3b62fcf4
Nate Anderson <nate.anderson@kellpro.com>
2012-11-28 22:09:41 -0600

    key_commit: added print loop

--- combinePDFs.bat
+++ combinePDFs.bat
@@ -1,13 +1,35 @@
 @@echo off
 set gs="C:\Program Files\Bullzip\PDF Printer\gs\gswin32c.exe"
 set tempFile="..\aaatempFile.txt"
-set scratch="c:\client\aaa.pdf"
-set final="..\final.pdf"
+set scratch=c:\client\aaa.pdf
+set final=c:\client\final.pdf
 set pdfFolder="c:\client\taxStatements"
 set pdfFolderEsc=%pdfFolder:\=\\%
+set options="-q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite"
 
-echo -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=%scratch% > %tempfile%
-dir %pdfFolder%\*.pdf /b /on | gawk -v cmd="%pdfFolderEsc%\\" "{print cmd$0}" >> %tempFile%
+for /R %pdfFolder% %%i IN (*.*) DO (
+    %gs% -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=%final% %scratch% %%i
+    echo ren %final% aaa.pdf
+)
+rem set nextFileName=
 
-%gs% @%tempFile%
-del %tempFile%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+rem echo -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=%scratch% > %tempfile%
+rem dir %pdfFolder%\*.pdf /b /on | gawk -v cmd="%pdfFolderEsc%\\" "{print cmd$0}" >> %tempFile%
+
+rem %gs% @%tempFile%
+rem del %tempFile%
