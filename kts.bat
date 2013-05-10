2f639e24dde956207a885a3f5cf9ed6f8dfa49fd
Nate Anderson <nate.anderson@kellpro.com>
2013-05-10 10:30:17 -0500

    added check for python

--- kts.bat
+++ kts.bat
@@ -1,1 +1,18 @@
+@echo off
 python kts.py %1
+if ERRORLEVEL = 1 GOTO NoPython
+GOTO END
+
+:NoPython
+echo    You will need to install Active Python...
+echo       http://www.activestate.com/activepython/downloads
+set /p yesorno="Would you like to open this website?: " %=%
+IF /I "%yesorno%"=="Y" call:StartUrl
+IF /I "%yesorno%"=="YES" call:StartUrl
+GOTO END
+
+:StartUrl
+start http://www.activestate.com/activepython/downloads
+GOTO END
+
+:END
