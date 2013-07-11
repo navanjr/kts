217581c96be85db39b1f954da506176f52427e6e
Jeremy <jeremy.shepherd@kellpro.com>
2013-07-11 11:54:51 -0500

    key_commit: #2091 - conversion of paycodes needs to also create...

--- functions.py
+++ functions.py
@@ -347,7 +347,9 @@ class ktsMenu():
             print 'journalTypes...', self.sqlAlterProc('journalTypes','TableFunction','99')
             print 'glCreateTables...', self.sqlAlterProc('glCreateTables')
             print 'Run glCreateTables...', self.sqlQuery('exec dbo.glCreateTables',True)['code']
-            print 'createPayCodes...', self.sqlAlterProc('createPayCodes','Procedure','99')
+            print 'glAccounts...', self.sqlAlterProc('glAccounts','View','99')
+            print 'glAccountVerification...', self.sqlAlterProc('glAccountVerification','Procedure','99')
+            print 'creates...', self.sqlAlterProc('createPayCodes','Procedure','99')
             print 'Run createPayCodes...', self.sqlQuery('exec dbo.createPayCodes',True)['code']
             print 'diagnostics...', self.sqlAlterProc('diagnostics','Procedure','99')
             print 'dir...', self.sqlAlterProc('dir','TableFunction')
