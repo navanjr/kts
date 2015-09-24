9a1f0859f17de265701a16b44b67ee838dfb05cd
Matthew Keller <matt.keller@kellpro.com>
2015-09-24 08:49:31 -0500

    key_commit: Added the ability to print tax statements from crystal reports

--- dkts.bat
+++ dkts.bat
@@ -1,6 +1,6 @@
 if exist ukts.exe del ukts.exe
   
-pkzipc -add -sfx -dir ..\ukts c:\client\key\kts\templates\*.* c:\client\key\kts\sqlobjects\*.* c:\client\key\kts\kts.bat c:\client\key\kts\*.py c:\client\key\kts\*.ftp
+pkzipc -add -sfx -dir ..\ukts c:\client\key\kts\reports\*.* c:\client\key\kts\templates\*.* c:\client\key\kts\sqlobjects\*.* c:\client\key\kts\kts.bat c:\client\key\kts\*.py c:\client\key\kts\*.ftp
  
 rem ftp -n -s:kts.ftp 
 copy ..\ukts.exe \\192.168.1.14\ftp.kellpro.com\pub\updates
